ae32b30df0aef117fe0a02ac8b599958b4c9a68d
Sarayut60-068 <s6035512068@phuket.psu.ac.th>
2020-03-19 23:32:32 +0700

    text

---
+++ Lab8-Firebase-240311/todo/src/config.js
@@ -0,0 +1,14 @@
+    const config = {
+        apiKey: "AIzaSyBS43f7Ey2cvX1jglzJ8_LmBf1AfxBmvwA",
+        authDomain: "todo-97edb.firebaseapp.com",
+        databaseURL: "https://todo-97edb.firebaseio.com",
+        projectId: "todo-97edb",
+        storageBucket: "todo-97edb.appspot.com",
+        messagingSenderId: "180910252848",
+        appId: "1:180910252848:web:a5bda6f91ea7a3a00f1ef5",
+        measurementId: "G-SNFGVTLFW2"
+      };
+  
+
+  export default config
+
